38e02c8858124f6a8a5a600ec9233e64bc8285ea
Scott Clasen <scott@heroku.com>
2016-05-12 15:28:07 -0700

    use --latency

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM heroku/cedar:14
 
 WORKDIR /app
+RUN mkdir -p /app/user
 COPY ./ /app/
 RUN make 
--- wrk.sh
+++ wrk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
- echo /app/wrk -t $THREADS -c $CONNECTIONS -R $RATE -d $DURATION $URL
- /app/wrk -t $THREADS -c $CONNECTIONS -R $RATE -d $DURATION $URL
+ echo /app/wrk --latency -t $THREADS -c $CONNECTIONS -R $RATE -d $DURATION $URL
+ /app/wrk --latency -t $THREADS -c $CONNECTIONS -R $RATE -d $DURATION $URL
  sleep 1
 done
